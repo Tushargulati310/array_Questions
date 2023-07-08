02a34027bb8899229aff38cd15660fc3a06d3bf0
Tushar Gulati <123962105+Tushargulati310@users.noreply.github.com>
2023-07-08 23:30:47 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,16 @@
+class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                majority = nums[i];
+                count = 1;
+            } else if (nums[i] == majority) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return majority;
+    }
